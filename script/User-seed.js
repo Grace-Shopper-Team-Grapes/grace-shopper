f10b6083631632313cf47cf1a07dda94c57363ca
mh-393 <55870690+mh-393@users.noreply.github.com>
2020-01-14 17:11:12 -0500

    changed associations and defined user seed

---
+++ script/User-seed.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    firstName: 'Jim',
+    lastName: 'Bob',
+    email: 'jim.bob@gmail.com',
+    phone: '1234567890',
+    password: '123',
+    googleId: 'bobo'
+  },
+  {
+    firstName: 'Harold',
+    lastName: 'Smith',
+    email: 'hsmith@gmail.com',
+    phone: '0987654321',
+    password: '456',
+    googleId: 'hobo'
+  }
+];
