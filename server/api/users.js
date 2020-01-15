703a380d616dc300eb663784e70d9ea0be9a284b
Ziv <zkarmi25@gmail.com>
2020-01-15 18:43:57 -0500

    Moved API /account route into /users, removed /account

--- server/api/users.js
+++ server/api/users.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
-const {User} = require('../db/models');
+const {User, Order} = require('../db/models');
 module.exports = router;
 
+// Get all users - ADMIN ONLY
 router.get('/', async (req, res, next) => {
   try {
     const users = await User.findAll({
@@ -16,11 +17,53 @@ router.get('/', async (req, res, next) => {
   }
 });
 
+// Get individual user
 router.get('/:id', async (req, res, next) => {
   try {
-    const specificUser = await User.findByPk(req.params.id);
+    const specificUser = await User.findByPk(req.params.id, {
+      attributes: {
+        exclude: ['password', 'salt']
+      }
+    });
     res.json(specificUser);
   } catch (e) {
     next(e);
+  }
+});
+
+// Get all orders by a user
+router.get('/:id/orders', async (req, res, next) => {
+  try {
+    const orders = await Order.findAll({
+      where: {
+        userId: req.params.id
+      }
+    });
+    res.json(orders);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// edit user
+router.put('/:id', async (req, res, next) => {
+  try {
+    const updatedStatus = await User.update(
+      {
+        phone: req.body.phone
+      },
+      {where: {id: req.params.id}}
+    );
+    //if we choose to send back updated User with new
+    //phone number
+    // const specificUser = await User.findByPk(req.params.id);
+    // if (updatedStatus === 0) res.sendStatus(500);
+    // else {
+    //   res.json(specificUser).status(202);
+    // }
+    if (updatedStatus === 0) res.sendStatus(500);
+    else res.sendStatus(204);
+  } catch (error) {
+    next(error);
   }
 });
