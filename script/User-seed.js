38ad7cf0dbf5e54414b514adb5042c5b17386051
adamvare <adam44@me.com>
2020-01-15 19:08:55 -0500

    cleanup in order to run

--- script/User-seed.js
+++ script/User-seed.js
@@ -4,6 +4,10 @@ module.exports = [
     lastName: 'Bob',
     email: 'jim.bob@gmail.com',
     phone: '1234567890',
+    street1: '1230 funfun lane',
+    city: 'new york',
+    state: 'new york',
+    zip: '12345',
     password: '123',
     googleId: 'bobo'
   },
@@ -12,6 +16,10 @@ module.exports = [
     lastName: 'Smith',
     email: 'hsmith@gmail.com',
     phone: '0987654321',
+    street1: 'treasure island',
+    city: 'island',
+    state: 'ocean',
+    zip: '54321',
     password: '456',
     googleId: 'hobo'
   }
