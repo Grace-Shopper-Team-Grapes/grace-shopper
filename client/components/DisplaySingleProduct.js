6f1684a52161d89f9eaffd226684ebda0f236f0e
adamvare <adam44@me.com>
2020-01-17 14:30:46 -0500

    singleProductComponent + redux working

---
+++ client/components/DisplaySingleProduct.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {getSingleProduct} from '../store/singleProduct';
+import {Link} from 'react-router-dom';
+
+class SingleProduct extends React.Component {
+  componentDidMount() {
+    this.props.getSingleProduct();
+  }
+
+  render() {
+    if (!this.props.product) {
+      return <h1>loading singleProduct...</h1>;
+    } else {
+      const product = this.props.product;
+      return (
+        <div>
+          <h1>{product.name}</h1>
+          <h2>{product.price}</h2>
+          <h3>{product.inventory}</h3>
+        </div>
+      );
+    }
+  }
+}
+
+const mapState = state => {
+  return {
+    product: state.product
+  };
+};
+
+const mapDispatch = (dispatch, ownProps) => {
+  return {
+    getSingleProduct: () => {
+      dispatch(getSingleProduct(ownProps.match.params.slug));
+    }
+  };
+};
+
+export default connect(mapState, mapDispatch)(SingleProduct);
