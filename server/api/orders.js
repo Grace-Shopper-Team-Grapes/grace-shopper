8d81fce87ce4585e982d6e4030f61d7bb277a348
adamvare <adam44@me.com>
2020-01-18 10:52:06 -0500

    checkout and shipped route working

--- server/api/orders.js
+++ server/api/orders.js
@@ -6,7 +6,6 @@ router.post('/', async (req, res, next) => {
   try {
     const pid = req.body.pid;
     const pqty = req.body.pqty;
-
     const product = await Product.findByPk(pid);
     if (product.inventory > pqty) {
       res.json(product);
@@ -36,6 +35,32 @@ router.put('/:pid/:pqty', async (req, res, next) => {
     }
   } catch (e) {
     next(e);
+  }
+});
+
+//CHECKOUT
+router.get('/checkout/:id', async (req, res, next) => {
+  try {
+    const order = await Order.findByPk(req.params.id);
+    order.update({
+      isPurchased: true
+    });
+    res.redirect('/products');
+  } catch (error) {
+    next(error);
+  }
+});
+
+//SHIPPED
+router.get('/ship/:id', async (req, res, next) => {
+  try {
+    const order = await Order.findByPk(req.params.id);
+    order.update({
+      isShipped: true
+    });
+    res.redirect('/products');
+  } catch (error) {
+    next(error);
   }
 });
 
