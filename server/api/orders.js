0d0a023396479b1d502c2128100bad2854971d98
adamvare <adam44@me.com>
2020-01-19 19:49:23 -0500

    getAllOrders and getAllOrderProduct routes made

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,40 +1,17 @@
 const router = require('express').Router();
 const {User, Order, Product, OrderProduct} = require('../db/models');
-const {Sequelize} = require('sequelize');
 
-router.post('/', async (req, res, next) => {
+//GET ALL ORDERS
+router.get('/', async (req, res, next) => {
   try {
-    const pid = req.body.pid;
-    const pqty = req.body.pqty;
-    const product = await Product.findByPk(pid);
-    if (product.inventory > pqty) {
-      res.json(product);
-    }
-  } catch (e) {
-    next(e);
-  }
-});
-
-router.put('/:pid/:pqty', async (req, res, next) => {
-  try {
-    const pid = req.params.pid;
-    const pqty = req.params.pqty;
-
-    const product = await Product.findOne({
+    const allOrders = await Order.findAll({
       where: {
-        id: pid,
-        inventory: {
-          $lte: pqty
-        }
+        userId: req.user.id
       }
     });
-    if (product) {
-      res.json(pqty);
-    } else {
-      throw new Error('Not enough product available.');
-    }
-  } catch (e) {
-    next(e);
+    res.json(allOrders);
+  } catch (error) {
+    next(error);
   }
 });
 
