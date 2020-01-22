8ed4de00951337c5f9183b358b49d322aecbee9c
Ziv <zkarmi25@gmail.com>
2020-01-22 11:32:41 -0500

    Minor footer tweaks

--- client/components/template/Footer.js
+++ client/components/template/Footer.js
@@ -25,49 +25,18 @@ const Footer = () => (
           </li>
         </ul>
       </div>
-      <div className="footer-links-column grid-item--one-fourth">
-        <strong className="footer-links-column__title">
-          A Footer Links List
-        </strong>
-        <ul className="footer-links-column__list">
-          <li className="footer-links-column__link">
-            <a href="#">A footer link</a>
-          </li>
-          <li className="footer-links-column__link">
-            <a href="#">A footer link</a>
-          </li>
-          <li className="footer-links-column__link">
-            <a href="#">A footer link</a>
-          </li>
-          <li className="footer-links-column__link">
-            <a href="#">A footer link</a>
-          </li>
-          <li className="footer-links-column__link">
-            <a href="#">A footer link</a>
-          </li>
-        </ul>
-      </div>
-      <div className="footer-links-column grid-item--one-fourth">
-        <strong className="footer-links-column__title">
-          A Footer Links List
-        </strong>
-        <ul className="footer-links-column__list">
-          <li className="footer-links-column__link">
-            <a href="#">A footer link</a>
-          </li>
-          <li className="footer-links-column__link">
-            <a href="#">A footer link</a>
-          </li>
-          <li className="footer-links-column__link">
-            <a href="#">A footer link</a>
-          </li>
-          <li className="footer-links-column__link">
-            <a href="#">A footer link</a>
-          </li>
-          <li className="footer-links-column__link">
-            <a href="#">A footer link</a>
-          </li>
-        </ul>
+      <div className="footer-project grid-item--one-half">
+        <p>
+          <strong className="footer-links-column__title">
+            GrapesShopper &ndash;
+          </strong>{' '}
+          An E-Commerce Platform
+        </p>
+        <p className="footer__by-line">
+          by <a href="#">Kenneth Chen</a>, <a href="#">Marvin Huang</a>,&nbsp;
+          <a href="#">Ziv Karmi</a>, and&nbsp;
+          <a href="#">Adam Vare</a>
+        </p>
       </div>
       <div className="footer-links-column grid-item--one-fourth">
         <strong className="footer-links-column__title">
